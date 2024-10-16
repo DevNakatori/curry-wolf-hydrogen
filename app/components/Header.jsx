5400b9cb1c7e2e43e4a7deb007ee155ef9306739
DevNakatori <dev@nakatori.agency>
2024-10-16 11:58:30 +0530

    add prefeatch css

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -140,6 +140,7 @@ export function HeaderMenu({
               <div className="menu-item-wrapper">
                 <NavLink
                   end
+                  prefetch="intent"
                   className={({isActive}) => (isActive ? 'active' : '')}
                   to={url}
                   onClick={(e) => handleClick(e, url)}
@@ -188,6 +189,7 @@ export function HeaderMenu({
                         <NavLink
                           key={index}
                           end
+                          prefetch="intent"
                           className={({isActive}) => (isActive ? 'active' : '')}
                           to={url}
                           onClick={(e) => handleClick(e, url)}
