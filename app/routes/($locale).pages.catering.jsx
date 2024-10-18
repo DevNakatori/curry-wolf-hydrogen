0a009cb4196a5d3dddf9db1b11c893d77f4d10a6
DevNakatori <dev@nakatori.agency>
2024-10-18 17:42:52 +0530

    fix  mobile slider in catering page

--- app/routes/($locale).pages.catering.jsx
+++ app/routes/($locale).pages.catering.jsx
@@ -106,6 +106,58 @@ export default function Page() {
       window.removeEventListener('resize', setEqualHeight);
     };
   }, [location]);
+  useEffect(() => {
+    if (window.innerWidth < 768) {
+      setTimeout(function () {
+        const sliderContainer = document.querySelector('.ref-wrap');
+        const slides = document.querySelectorAll('.ref-box');
+        let currentIndex = 0;
+        let slidesToShow = 1;
+
+        function updateSlider() {
+          if (window.innerWidth < 768) {
+            slidesToShow = 1.2;
+          } else {
+            slidesToShow = 1;
+          }
+
+          const width = sliderContainer.clientWidth / slidesToShow;
+          slides.forEach((slide) => {
+            slide.style.minWidth = `${width}px`;
+          });
+          sliderContainer.style.transform = `translateX(${
+            -width * currentIndex
+          }px)`;
+        }
+
+        function nextSlide() {
+          if (currentIndex < slides.length - slidesToShow) {
+            currentIndex += 1;
+          } else {
+            currentIndex = 0;
+          }
+          updateSlider();
+        }
+
+        function startAutoplay() {
+          setInterval(nextSlide, 3000);
+        }
+
+        function resetAutoplay() {
+          clearInterval(autoplayInterval);
+          startAutoplay();
+        }
+
+        window.addEventListener('resize', function () {
+          updateSlider();
+          currentIndex = 0;
+        });
+
+        updateSlider();
+        startAutoplay();
+      }, 2000);
+    }
+  }, []);
 
   const {locale} = LoaderData();
   const cateringPageImages = data?.cateringPageImages;
